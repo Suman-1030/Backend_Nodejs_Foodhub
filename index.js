1e04b257f84ef29187753733932197bf72d0ec16
Suman Kagithala <kagithalasuman1030@gmail.com>
2025-10-26 10:41:53 +0530

    cors updated

--- index.js
+++ index.js
@@ -23,7 +23,19 @@ app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: true })); 
 
 
-app.use(cors());
+app.use((req, res, next) => {
+   
+    res.header("Access-Control-Allow-Origin", "https://frontend-ui-foodhub-omr53ysyv-sumans-projects-cb421f09.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); 
+    res.header("Access-Control-Allow-Credentials", "true"); 
+   
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+  
+    next();
+  });
 
 
 // Serve static files (uploads)
